2c6a8e0b6f04a84fb091e9fd152c8a3d6c9f6606
savjignesh <savaliya.jignesh20@gmail.com>
2015-04-11 22:24:31 +0530

    first commit

---
+++ models/Test.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "test".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $city
+ */
+class Test extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'test';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'city'], 'required'],
+            [['name', 'city'], 'string', 'max' => 50]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'city' => 'City',
+        ];
+    }
+}
