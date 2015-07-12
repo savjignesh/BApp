50854f242d3867508ef718c58a2421b27d5182ea
savjignesh <savaliya.jignesh20@gmail.com>
2015-07-13 00:12:26 +0530

    yii2 update

---
+++ views/bill/report.php
@@ -0,0 +1,40 @@
+<h2>Print Bill</h2>
+<hr>
+
+<table style="width:100%;">
+	
+	<tr>
+		<td>Customer</td>
+		<td><?= $model->customer_Id; ?></td>
+		<td>Net Amount</td>
+		<td><?= $model->net_amount; ?></td>
+	</tr>
+	<tr>
+		<td>Total Items</td>
+		<td><?= $model->total_items; ?></td>
+		<td>bill_date</td>
+		<td><?= $model->bill_date; ?></td>
+	</tr>
+</table>
+<hr>
+<table style="width:100%;">
+	
+	<tr style="background: #000; color: #fff;">
+		<td></td>
+		<td>Item</td>
+		<td>qty</td>
+		<td>price</td>
+	</tr>
+	<?php $i=1; ?>
+	<?php foreach ($data as $value) {
+		
+		echo '<tr style="background: #ddd;">';
+			echo '<td>'.$i.'</td>';
+			echo '<td>'.$value->item_Id.'</td>';
+			echo '<td>'.$value->qty.'</td>';
+			echo '<td>'.$value->price.'</td>';
+		echo '</tr>';
+		$i++;
+	} ?>
+</table>
+
